bade43a4d4a71f227bb8d4a0ee34a2ed06955214
tnguyen0714 <126220644+tnguyen0714@users.noreply.github.com>
2023-09-21 13:39:32 -0400

    Add files via upload

---
+++ simple-functions/js/main.js
@@ -0,0 +1,22 @@
+//create a function that adds two numbers and alerts the sum
+function addNum (a,b) {
+    let sum = a + b;
+    alert(sum)
+}
+addNum(1,2)
+
+
+// //create a function that multiplys three numbers and console logs the product
+function multiplyNum(a, b, c){
+   let product = a * b * c;
+   console.log(product)
+}
+
+multiplyNum(1, 2, 3)
+
+// //create a function that divides two numbers and returns the ???
+function divideNum(a, b){
+    let divideNum = a / b;
+    return(divideNum);
+}
+console.log(divideNum(4,2))
