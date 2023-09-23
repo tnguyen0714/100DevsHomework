d5fd8d95df907b328e86acfc3d721ec44ad671b3
tnguyen0714 <126220644+tnguyen0714@users.noreply.github.com>
2023-09-22 21:34:04 -0400

    Add files via upload

---
+++ bring-it-home-practice/js/main.js
@@ -0,0 +1,43 @@
+// *Variables*
+// Create a variable and console log the value
+    let a = 2;
+    console.log(a)
+// Create a variable, add 10 to it, and alert the value
+    let b = 2;
+    sum = b + 10;
+    console.log(sum)
+// *Functions*
+// Create a function that subtracts 4 numbers and alerts the difference
+    function practice(){
+        let num1 = 1; 
+        let num2 = 2;
+        let num3 = 3;
+        let num4 = 6;
+        let difference = num1 - num2 - num3 - num4;
+    }
+    alert(practice)
+// Create a function that divides one number by another and returns the remainder
+    function practice2(){
+        let num5 = 6
+        let num6 = 11
+        let remainder = num6/num5
+    }
+    return(pracetice2)
+// *Conditionals*
+// Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+    function conditionals1(){
+        let num7 = 20
+        let num8 = 40
+        let jumanji = num7 + num8
+    alert(conditionals1)
+    }
+// Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+    function conditionals2(){
+        let num9 = 9;
+        let num10 = 10;
+        let num11 = 11;
+        let product = (num9 * num10 * num11)/3;
+        if (a == product/3){
+            alert(ZEBRA)
+        }
+    }
