d63c8ff565cfd92335560ae19b48e8453ee49b76
tnguyen0714 <126220644+tnguyen0714@users.noreply.github.com>
2023-10-10 18:24:12 -0400

    Add files via upload

---
+++ review-in-paradise-after-paradise/js/main.js
@@ -0,0 +1,12 @@
+// Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+let numbers = [1, 2, 3, 4, 5]
+
+function lessThanArray(numbers){
+    if(numbers[0] < numbers[length.numbers - 1]){
+        alert('Hi')
+    }else if(number[0] > numbers[length.numbers - 1]){
+        alert('Bye')
+    }else{
+        alert('We close in an hour')
+    }
+}
