4b494d4beeecb54997407308a297c3d269203f76
tnguyen0714 <126220644+tnguyen0714@users.noreply.github.com>
2023-10-18 15:06:07 -0400

    Add files via upload

---
+++ objects-lost-galaxy/js/main.js
@@ -0,0 +1,15 @@
+//Create a mouse object that has four properties and three methods
+function mouse(name, food, friend, color){
+    character.nameOfAnimal = name
+    character.favoriteFood = food
+    character.bestFriend = friend
+    character.favoriteColor = color
+    character.fear = function(){
+        alert('This is a trap!')
+    }
+    character.tv = function(){
+        alert('Tom and Jerry is my favorite TV show!')
+    }
+
+
+}
